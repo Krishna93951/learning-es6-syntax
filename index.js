ea50bd9814751145983ac5faa4bfe058bc7640bd
Krishna93951 <kkagile123@gmail.com>
2019-12-17 11:36:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,45 +3,14 @@ import "./style.css";
 
 // Write Javascript code!
 
-//class
-class Food{
-  constructor(type,dish){
-    this.type = type;
-    this.dish = dish;
-  }
-  taste(){
-    console.log(`${this.type} dishes like ${this.dish} are very tasty`);
-  }
-}
+//promise
+let firstPromise = new Promise((resolve,reject) => {
+  setTimeout(()=>{
+    resolve('Success!');
+  },1000);
+})
 
-class Indian extends Food{
-  construnctor(type,dish){
-    super(type,dish);
-  }
-  taste(){
-    console.log(`${this.type} dishes like ${this.dish} are very tasty`);
-  }
-}
+firstPromise.then((successMessage)=>{
+  console.log(`Ya ${successMessage}`);
+})
 
-var meal = new Indian('Indian','Roti');
-meal.taste();
-
-//Destructuring Assignment
-let list = [1,2,3];
-let [a,b] = list;
-console.log(a,b);
-
-// var object1 = {
-// method1(){
-// console.log('Hello World');
-// }
-// }
-
-// var object2 = {
-// method2(){
-// super.method1();
-// }
-// }
-
-// Object.setPrototypeOf(object2,object1);
-// object2.method2()
